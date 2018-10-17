278133f91f45db39b58331ccc0fc4f3626e5b784
Rafael Martins <rafael.martins@teamwork.com>
2018-10-17 17:47:22 -0300

    Fix linter warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,63 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    watch: true,
+  watch: true,
 
-    eslint: {
-        configFile: '.eslintrc.js'
-    },
+  eslint: {
+    configFile: '.eslintrc.js'
+  },
 
-    entry: [
-        './src/app/app.js'
+  entry: [
+    './src/app/app.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'app.js'
+  },
+
+  // use inline source maps.
+  devtool: 'cheap-module-source-map',
+
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: /node_modules/,
+      loaders: ['ng-annotate', 'babel-loader', 'eslint-loader']
+    }, {
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.html$/,
+      loader: 'html'
+    }]
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'node_modules')
     ],
+    extensions: ['', '.js']
+  },
 
-    output: {
-        path: __dirname + '/www',
-        filename: 'app.js'
-    },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
 
-    // use inline source maps.
-    devtool: 'cheap-module-source-map',
+      // load our index.html "template"
+      template: './src/index.html',
 
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            include: [
-                path.resolve(__dirname, 'src')
-            ],
-            exclude: /node_modules/,
-            loaders: ['ng-annotate', 'babel-loader', 'eslint-loader']
-        }, {
-            test: /\.js$/,
-            include: [
-                path.resolve(__dirname, 'src')
-            ],
-            loader: 'eslint-loader',
-            exclude: /node_modules/
-        },{
-            test: /\.html$/,
-            loader: 'html'
-        }]
-    },
-    resolve: {
-        root: [
-            path.join(__dirname, 'node_modules')
-        ],
-        extensions: ['', '.js']
-    },
-
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new HtmlWebpackPlugin({
-
-            // load our index.html "template"
-            template: './src/index.html',
-
-            // inject all scripts into the body
-            inject: 'body',
-            filename: 'index.html'
-        })
-    ]
-};
+      // inject all scripts into the body
+      inject: 'body',
+      filename: 'index.html'
+    })
+  ]
+}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,56 +1,56 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    watch: false,
+  watch: false,
 
-    entry: [
-        './src/app/app.js'
+  entry: [
+    './src/app/app.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'app.js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /(test|node_modules|\.spec\.js$)/,
+      loaders: ['ng-annotate', 'babel-loader']
+    }, {
+      test: /\.html$/,
+      loader: 'html'
+    }]
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'node_modules')
     ],
+    extensions: ['', '.js']
+  },
 
-    output: {
-        path: __dirname + '/www',
-        filename: 'app.js'
-    },
+  plugins: [
+    new HtmlWebpackPlugin({
 
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /(test|node_modules|\.spec\.js$)/,
-            loaders: ['ng-annotate', 'babel-loader']
-        },{
-            test: /\.html$/,
-            loader: 'html'
-        }]
-    },
-    resolve: {
-        root: [
-            path.join(__dirname, 'node_modules')
-        ],
-        extensions: ['', '.js']
-    },
+      // load our index.html "template"
+      template: './src/index.html',
 
-    plugins: [
-        new HtmlWebpackPlugin({
-
-            // load our index.html "template"
-            template: './src/index.html',
-
-            // inject all scripts into the body
-            inject: 'body',
-            filename: 'index.html'
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
-    ]
-};
+      // inject all scripts into the body
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+}
